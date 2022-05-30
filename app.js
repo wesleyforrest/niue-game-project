b16382892986aaab582c0cbe9410fd9778700190
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-30 15:33:17 +0100

    added DOM connections targeting classes and ids requred

---
+++ app.js
@@ -0,0 +1,8 @@
+const questionNumber = document.querySelector(".question-number");
+const question = document.querySelector(".question");
+const imageCard = document.querySelector(".image-container__card");
+
+const answerA = document.querySelector("#answer-a");
+const answerB = document.querySelector("#answer-b");
+const answerC = document.querySelector("#answer-c");
+const answerD = document.querySelector("#answer-d");
