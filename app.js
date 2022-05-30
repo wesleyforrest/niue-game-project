9fa82e1862b165008be46054c808a0a64f9f4a5e
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-30 17:13:41 +0100

    added images and questions object

--- app.js
+++ app.js
@@ -6,3 +6,76 @@ const answerA = document.querySelector("#answer-a");
 const answerB = document.querySelector("#answer-b");
 const answerC = document.querySelector("#answer-c");
 const answerD = document.querySelector("#answer-d");
+
+const questions = [
+  {
+    number: 1,
+    text: "Which team won the 2021 America's Cup?",
+    image: "",
+    answer: "New Zealand",
+  },
+
+  {
+    number: 2,
+    text: "Which band was responsible for the song 'Down Under'",
+    image: "",
+    answer: "Men at work",
+  },
+
+  {
+    number: 3,
+    text: "Who skippered the 2021 America's Cup UK team?",
+    image: "",
+    answer: "Ben Ainslie",
+  },
+
+  {
+    number: 4,
+    text: "What is this sail called?",
+    image: "",
+    answer: "Spinnaker",
+  },
+
+  {
+    number: 5,
+    text: "Which Island hosts Cowes Week?",
+    image: "",
+    answer: "Cowes",
+  },
+
+  {
+    number: 6,
+    text: "In which year was the first America's Cup?",
+    image: "",
+    answer: 1851,
+  },
+
+  {
+    number: 7,
+    text: "Sir Robin Knox-Johnson is famous for achieving what?",
+    image: "",
+    answer:
+      "First person to perform a single-handed non-stop circumnavigation of the globe",
+  },
+
+  {
+    number: 8,
+    text: "Which larger brand sponsors a sailing regatta in St Maarten",
+    image: "",
+    answer: "HEINEKEN",
+  },
+
+  {
+    number: 9,
+    text: "What is the left hand side of a boat called?",
+    image: "",
+    answer: "Port",
+  },
+
+  {
+    number: 10,
+    text: "WHat is the code name for Jeff Bezos' new sailing yacht?",
+    image: "",
+    answer: "OCEANCO Y721",
+  },
+];
