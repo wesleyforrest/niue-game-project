42580b060b12bb1aab5c5d9847c5f690c0f53a3b
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-31 14:59:22 +0100

    finished questions array

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import questionsArr from "./data/questions";
+
 const questionNumber = document.querySelector(".question-number");
 const question = document.querySelector(".question");
 const imageCard = document.querySelector(".image-container__card");
@@ -7,76 +9,17 @@ const answerB = document.querySelector("#answer-b");
 const answerC = document.querySelector("#answer-c");
 const answerD = document.querySelector("#answer-d");
 
-const questions = [
-  {
-    number: 1,
-    text: "Which team won the 2021 America's Cup?",
-    imgUrl:
-      "https://i.guim.co.uk/img/media/3a3cfecfe9369abfdc4ea5e01ec15ae9d34880d6/0_0_4369_2622/master/4369.jpg?width=620&quality=85&auto=format&fit=max&s=9b1726f6d6a5f44068c63d27ab47c71b",
-    answer: "New Zealand",
-  },
+const quizCards = (array) => {
+  const quizHTML = array.map((questionsArr) => {
+    return ` <h3 class="question-number">Question - ${}</h3>
+        <p class="question">What is this?</p>
+        <div class="image-container">
+          <div class="image-container__card" id="question-image-1"></div>
+          <!-- <div class="image-container=__card" id="question-image-2"></div>></div>
+          <div class="image-container=__card" id="question-image-3"></div>></div>
+          <div class="image-container=__card" id="question-image-4"></div>></div> -->
+        </div>`;
+  });
+};
 
-  {
-    number: 2,
-    text: "Which band was responsible for the song 'Down Under'",
-    image: "",
-    answer: "Men at work",
-  },
-
-  {
-    number: 3,
-    text: "Who skippered the 2021 America's Cup UK team?",
-    image: "",
-    answer: "Ben Ainslie",
-  },
-
-  {
-    number: 4,
-    text: "What is this sail called?",
-    image: "",
-    answer: "Spinnaker",
-  },
-
-  {
-    number: 5,
-    text: "Which Island hosts Cowes Week?",
-    image: "",
-    answer: "Cowes",
-  },
-
-  {
-    number: 6,
-    text: "In which year was the first America's Cup?",
-    image: "",
-    answer: 1851,
-  },
-
-  {
-    number: 7,
-    text: "Sir Robin Knox-Johnson is famous for achieving what?",
-    image: "",
-    answer:
-      "First person to perform a single-handed non-stop circumnavigation of the globe",
-  },
-
-  {
-    number: 8,
-    text: "Which larger brand sponsors a sailing regatta in St Maarten",
-    image: "",
-    answer: "HEINEKEN",
-  },
-
-  {
-    number: 9,
-    text: "What is the left hand side of a boat called?",
-    image: "",
-    answer: "Port",
-  },
-
-  {
-    number: 10,
-    text: "WHat is the code name for Jeff Bezos' new sailing yacht?",
-    image: "",
-    answer: "OCEANCO Y721",
-  },
-];
+console.log(questionsArr)
---
+++ data/questions.js
@@ -0,0 +1,83 @@
+const questionsArr = [
+  {
+    number: 1,
+    text: "Which team won the 2021 America's Cup?",
+    imgUrl:
+      "https://i.guim.co.uk/img/media/3a3cfecfe9369abfdc4ea5e01ec15ae9d34880d6/0_0_4369_2622/master/4369.jpg?width=620&quality=85&auto=format&fit=max&s=9b1726f6d6a5f44068c63d27ab47c71b",
+    answer: "New Zealand",
+  },
+
+  {
+    number: 2,
+    text: "Which band was responsible for the song 'Down Under'",
+    imageUrl:
+      "https://i.kym-cdn.com/entries/icons/original/000/029/212/down_under.png",
+    answer: "Men at work",
+  },
+
+  {
+    number: 3,
+    text: "Who skippered the 2021 America's Cup UK team?",
+    imageUrl:
+      "https://res.cloudinary.com/ineoslive/image/upload/w_282,c_fill,g_auto,f_auto,q_auto/v1612871193//globalassets/ineos-group/news/team-uk-americas-cup-inc-website-low-res.jpg",
+    answer: "Ben Ainslie",
+  },
+
+  {
+    number: 4,
+    text: "What is this sail called?",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b9/Amante_Choate_48_photo_D_Ramey_Logan.jpg",
+    answer: "Spinnaker",
+  },
+
+  {
+    number: 5,
+    text: "Which Island hosts Cowes Week?",
+    imageUrl:
+      "https://www.visitbritain.com/sites/default/files/styles/consumer_vertical_hero__1920x1080/public/paragraphs_bundles/hero/isle-of-wight-3_0.jpg?itok=z1E9-K3H",
+    answer: "Cowes",
+  },
+
+  {
+    number: 6,
+    text: "In which year was the first America's Cup?",
+    imageUrl: "https://www.sail-world.com/photos/misc2018/yysw203169.jpg",
+    answer: 1851,
+  },
+
+  {
+    number: 7,
+    text: "Sir Robin Knox-Johnson is famous for achieving what?",
+    imageUrl:
+      "https://cdn.goldengloberace.com/app/uploads/2017/06/Robin-Knox-Johnston-H02.jpg",
+    answer:
+      "First person to perform a single-handed non-stop circumnavigation of the globe",
+  },
+
+  {
+    number: 8,
+    text: "Which larger brand sponsors a sailing regatta in St Maarten",
+    imageUrl:
+      "https://media.cntraveler.com/photos/5967a39b96efb35a0f3796d2/16:9/w_2560%2Cc_limit/maho-beach-GettyImages-459241223.jpg",
+    answer: "HEINEKEN",
+  },
+
+  {
+    number: 9,
+    text: "What is the left hand side of a boat called?",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ship_positionLights.svg/220px-Ship_positionLights.svg.png",
+    answer: "Port",
+  },
+
+  {
+    number: 10,
+    text: "WHat is the code name for Jeff Bezos' new sailing yacht?",
+    imageUrl:
+      "https://photo-assets.superyachttimes.com/photo/209333/image/large-c973d38c7a5500502d1c62dbf6265a9b.jpg",
+    answer: "OCEANCO Y721",
+  },
+];
+
+export default questionsArr;
