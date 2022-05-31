1fb1b7f6d1019df45a78cbdb2676069ac31aa607
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-31 17:05:12 +0100

    added answers array of objects

---
+++ answers.js
@@ -0,0 +1,15 @@
+const answerArray = {
+  question1: [
+    { answer: "New Zealand", isCorrect: true },
+    { answer: "USA", iisCorrect: false },
+    { answer: "UK", isCorrect: false },
+    { answer: "Italy", isCorrect: false },
+  ],
+
+  question2: [
+    { answer: "Rod Stewart", isCorrect: false },
+    { answer: "Men at Work", iisCorrect: true },
+    { answer: "Duran Duran", isCorrect: false },
+    { answer: "U2", isCorrect: false },
+  ],
+};
