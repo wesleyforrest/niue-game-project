438a688d71eadabb21adfe916cb6600a1e88884b
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-05-31 16:34:01 +0100

    resized images for questions

--- data/questions.js
+++ data/questions.js
@@ -10,7 +10,7 @@ const questionsArr = [
   {
     number: 2,
     text: "Which band was responsible for the song 'Down Under'",
-    imageUrl:
+    imgUrl:
       "https://i.kym-cdn.com/entries/icons/original/000/029/212/down_under.png",
     answer: "Men at work",
   },
@@ -18,7 +18,7 @@ const questionsArr = [
   {
     number: 3,
     text: "Who skippered the 2021 America's Cup UK team?",
-    imageUrl:
+    imgUrl:
       "https://res.cloudinary.com/ineoslive/image/upload/w_282,c_fill,g_auto,f_auto,q_auto/v1612871193//globalassets/ineos-group/news/team-uk-americas-cup-inc-website-low-res.jpg",
     answer: "Ben Ainslie",
   },
@@ -26,7 +26,7 @@ const questionsArr = [
   {
     number: 4,
     text: "What is this sail called?",
-    imageUrl:
+    imgUrl:
       "https://upload.wikimedia.org/wikipedia/commons/b/b9/Amante_Choate_48_photo_D_Ramey_Logan.jpg",
     answer: "Spinnaker",
   },
@@ -34,7 +34,7 @@ const questionsArr = [
   {
     number: 5,
     text: "Which Island hosts Cowes Week?",
-    imageUrl:
+    imgUrl:
       "https://www.visitbritain.com/sites/default/files/styles/consumer_vertical_hero__1920x1080/public/paragraphs_bundles/hero/isle-of-wight-3_0.jpg?itok=z1E9-K3H",
     answer: "Cowes",
   },
@@ -42,14 +42,14 @@ const questionsArr = [
   {
     number: 6,
     text: "In which year was the first America's Cup?",
-    imageUrl: "https://www.sail-world.com/photos/misc2018/yysw203169.jpg",
+    imgUrl: "https://www.sail-world.com/photos/misc2018/yysw203169.jpg",
     answer: 1851,
   },
 
   {
     number: 7,
     text: "Sir Robin Knox-Johnson is famous for achieving what?",
-    imageUrl:
+    imgUrl:
       "https://cdn.goldengloberace.com/app/uploads/2017/06/Robin-Knox-Johnston-H02.jpg",
     answer:
       "First person to perform a single-handed non-stop circumnavigation of the globe",
@@ -58,7 +58,7 @@ const questionsArr = [
   {
     number: 8,
     text: "Which larger brand sponsors a sailing regatta in St Maarten",
-    imageUrl:
+    imgUrl:
       "https://media.cntraveler.com/photos/5967a39b96efb35a0f3796d2/16:9/w_2560%2Cc_limit/maho-beach-GettyImages-459241223.jpg",
     answer: "HEINEKEN",
   },
@@ -66,7 +66,7 @@ const questionsArr = [
   {
     number: 9,
     text: "What is the left hand side of a boat called?",
-    imageUrl:
+    imgUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ship_positionLights.svg/220px-Ship_positionLights.svg.png",
     answer: "Port",
   },
@@ -74,7 +74,7 @@ const questionsArr = [
   {
     number: 10,
     text: "WHat is the code name for Jeff Bezos' new sailing yacht?",
-    imageUrl:
+    imgUrl:
       "https://photo-assets.superyachttimes.com/photo/209333/image/large-c973d38c7a5500502d1c62dbf6265a9b.jpg",
     answer: "OCEANCO Y721",
   },
